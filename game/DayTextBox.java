be16f207f1738c88608b4d9d474727c8f2d394d1
Callum Lee <callum.lee@stfc.ac.uk>
2023-12-05 13:45:14 +0000

    characters displayed correctly

--- game/DayTextBox.java
+++ game/DayTextBox.java
@@ -12,6 +12,27 @@ public class DayTextBox extends Actor
      * Act - do whatever the DayTextBox wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    private String purpose;
+    
+    public DayTextBox(String purpose)
+    {
+        this.purpose = purpose;
+        
+        if (purpose == "title")
+        {
+            GreenfootImage image = this.getImage();
+            image.scale(image.getWidth() / 5, image.getHeight() / 6); 
+            this.setImage(image);
+        }
+        else if (purpose == "character")
+        {
+            GreenfootImage boxImage = this.getImage();
+            boxImage.scale(boxImage.getWidth() / 23, boxImage.getHeight() / 7);
+            this.setImage(boxImage);
+        }
+    }
+    
     public void act()
     {
         // Add your action code here.
--- game/TitleScreen.java
+++ game/TitleScreen.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
 
 /**
  * Write a description of class TitleScreen here.
@@ -12,6 +13,9 @@ public class TitleScreen extends World
      * Constructor for objects of class TitleScreen.
      * 
      */
+    private StartButton startButton = new StartButton();
+    private DayTextBox textBox = new DayTextBox("title");
+    
     public TitleScreen()
     {    
         // Create a new world with 750x500 cells with a cell size of 1x1 pixels.
@@ -39,15 +43,56 @@ public class TitleScreen extends World
         }
         
         // Make text box for title
-        DayTextBox textBox = new DayTextBox();
-        GreenfootImage image = textBox.getImage();
-        image.scale(image.getWidth() / 5, image.getHeight() / 6); 
-        textBox.setImage(image);
         textBox.getImage().drawImage(new GreenfootImage("Zombie Villager", 64, Color.WHITE, null), 100, 40);
         addObject(textBox, 375, 150);
         
         // Add start button
-        StartButton startButton = new StartButton();
         addObject(startButton, 375, 400);
+    }
+    
+    public void act()
+    {
+        // Show character select screen of start button is pressed
+        if (Greenfoot.mouseClicked(startButton))
+        {
+            characterSelect();
+        }
+    }
+    
+    public void characterSelect()
+    {
+        // Remove title screen objects
+        removeObject(startButton);
+        removeObject(textBox);
+        
+        textBox.getImage().clear();
+        textBox = new DayTextBox("title");
+        textBox.getImage().drawImage(new GreenfootImage("Select Your Character", 64, Color.WHITE, null), 30, 40);
+        addObject(textBox, 375, 85);
+        
+        // Create array list to store all images for character select
+        ArrayList<GreenfootImage> images = new ArrayList<GreenfootImage>();
+        for (int i = 2; i <= 6; i++)
+        {
+            if (i != 3)
+            {
+                images.add(new GreenfootImage(i + " - forward facing.png"));
+            }
+        }
+        
+        ArrayList<DayTextBox> boxes = new ArrayList<DayTextBox>();
+        for (int i = 0; i <=3; i++)
+        {
+            DayTextBox charBox = new DayTextBox("character");
+            GreenfootImage char1 = images.get(i);
+            char1.scale(char1.getWidth() * 5, char1.getHeight() * 5);
+            charBox.getImage().drawImage(char1, 25, 20);
+            boxes.add(charBox);
+        }
+        
+        addObject(boxes.get(0), 250, 250);
+        addObject(boxes.get(1), 500, 250);
+        addObject(boxes.get(2), 250, 400);
+        addObject(boxes.get(3), 500, 400);
     }
 }
